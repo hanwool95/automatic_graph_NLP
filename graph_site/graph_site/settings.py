6752265fddb3205261af33375916756ad5b47398
hanwool <hanwool95@snu.ac.kr>
2021-09-17 15:25:41 +0900

    secrete

--- graph_site/graph_site/settings.py
+++ graph_site/graph_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secrete import secrete
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--l*!2z-4l#-hj(sk_xd#^*_5zh#*97o-91@+n+gn-ju8!c5p29'
+SECRET_KEY = secrete
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
